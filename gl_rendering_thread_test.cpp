3ec8de87cba9da00255b0e7bb4ff3038470b5382
Dimaster <gordeev.dmitriy@physics.msu.ru>
2023-09-05 13:10:27 +0300

    RenderThread receives render data

--- gl_rendering_thread_test.cpp
+++ gl_rendering_thread_test.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <gl/glew.h>
 #include <SDL.h>
+#include <mutex>
 
 #undef main
 
@@ -55,19 +56,30 @@ public:
 
             prepareCanvas();
 
-            createGeometry();
+            // createGeometry();
 
-            // TODO: Geometry - VBO, VAO ? - один раз вначале
+            while(!m_Running) {
+                Logger::info("Blocked");
+                if (m_ShouldAcceptGeometry) {
+                    m_Mutex.lock();
+
+                    createGeometry();
+                    m_ShouldAcceptGeometry = false;
+
+                    m_Mutex.unlock();
+                }
+            }
 
             while (gameState != GameState::EXIT) {
                 glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
 
-                glUseProgram(m_Shader->getShaderProgramId());
-
-                glBindVertexArray(m_VAO);
-                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-                glBindVertexArray(0);
+                if (m_VAO) {
+                    glUseProgram(m_Shader->getShaderProgramId());
+                    glBindVertexArray(m_VAO);
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+                    glBindVertexArray(0);
+                }
 
                 // swap buffers and draw everything on the screen
                 SDL_GL_SwapWindow(window);
@@ -92,6 +104,11 @@ public:
 
     RenderingThread(const RenderingThread &) = delete;
 
+
+    void startRenderingLoop() {
+        m_Running = true;
+    }
+
     void prepareCanvas() {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glEnable(GL_BLEND);
@@ -115,69 +132,24 @@ public:
         }
     }
 
-
     void createGeometry() {
-        // Geometry =====================================================
-        Vertex* geometry = new Vertex[4];
-        glm::vec2 m_Position = {0.0f, 0.0f};
-        glm::vec2 m_WorldSize = {1.0f, 1.0f};
-
-        // top left
-        geometry[0].pos.x = m_Position.x - m_WorldSize.x / 2.0f;
-        geometry[0].pos.y = m_Position.y + m_WorldSize.y / 2.0f;
-        geometry[0].color.r = 0;
-        geometry[0].color.g = 20;
-        geometry[0].color.b = 0;
-        geometry[0].uv.u = 0.0f;
-        geometry[0].uv.v = 1.0f;
-
-        // bottom left
-        geometry[1].pos.x = m_Position.x - m_WorldSize.x / 2.0f;
-        geometry[1].pos.y = m_Position.y - m_WorldSize.y / 2.0f;
-        geometry[1].color.r = 255;
-        geometry[1].color.g = 255;
-        geometry[1].color.b = 255;
-        geometry[1].uv.u = 0.0f;
-        geometry[1].uv.v = 0.0f;
-
-        // bottom right
-        geometry[2].pos.x = m_Position.x + m_WorldSize.x / 2.0f;
-        geometry[2].pos.y = m_Position.y - m_WorldSize.y / 2.0f;
-        geometry[2].color.r = 255;
-        geometry[2].color.g = 255;
-        geometry[2].color.b = 255;
-        geometry[2].uv.u = 1.0f;
-        geometry[2].uv.v = 0.0f;
-
-        // top right
-        geometry[3].pos.x = m_Position.x + m_WorldSize.x / 2.0f + 0.1f;
-        geometry[3].pos.y = m_Position.y + m_WorldSize.y / 2.0f;
-        geometry[3].color.r = 255;
-        geometry[3].color.g = 255;
-        geometry[3].color.b = 255;
-        geometry[3].uv.u = 1.0f;
-        geometry[3].uv.v = 1.0f;
-
-        int* indices = new int[6];
-        indices[0] = 3;
-        indices[1] = 0;
-        indices[2] = 1;
-        indices[3] = 1;
-        indices[4] = 2;
-        indices[5] = 3;
+        if (!m_Geometry || !m_Indices) {
+            Logger::error("RenderThread.createGeometry() : Geometry or Indices are null");
+            return;
+        }
 
         glGenVertexArrays(1, &m_VAO);
         glBindVertexArray(m_VAO);
 
-        GLuint vbo;
-        glGenBuffers(1, &vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), geometry, GL_STATIC_DRAW);
+//        GLuint vbo;
+        glGenBuffers(1, &m_VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+        glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), m_Geometry, GL_STATIC_DRAW);
 
-        GLuint ibo;
-        glGenBuffers(1, &ibo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(int), indices, GL_STATIC_DRAW);
+//        GLuint ibo;
+        glGenBuffers(1, &m_IBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(int), m_Indices, GL_STATIC_DRAW);
 
         // =====================================================================================
         // Attributes
@@ -193,8 +165,7 @@ public:
         glVertexAttribPointer(uvAttr, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)12);
         glEnableVertexAttribArray(uvAttr);
 
-        delete[] geometry;
-
+        // --------------------------------------------------------------------------------
         // Send shader's texture to uniform
         glUseProgram(m_Shader->getShaderProgramId());
         glActiveTexture(GL_TEXTURE0);
@@ -206,6 +177,17 @@ public:
         glUniform1i(textureLocation, 0);
     }
 
+    void addObject(Vertex* geometry, int* indices) {
+        m_Mutex.lock();
+        if (!geometry || !indices)
+            return;
+
+        m_ShouldAcceptGeometry = true;
+        m_Geometry = geometry;
+        m_Indices = indices;
+        m_Mutex.unlock();
+    }
+
 
 
 protected:
@@ -214,7 +196,16 @@ protected:
     std::thread m_Thread;
     std::exception_ptr m_ExceptionPtr;
     Shaders *m_Shader{nullptr};
+
+    bool m_Running {false};
+    bool m_ShouldAcceptGeometry {false};
+    Vertex* m_Geometry {nullptr};
+    int* m_Indices {nullptr};
+    std::mutex m_Mutex;
+
     GLuint m_VAO {0};
+    GLuint m_VBO {0};
+    GLuint m_IBO {0};
 };
 
 std::shared_ptr<class Logger> Logger::m_Instance = nullptr;
@@ -238,6 +229,57 @@ void handleInput() {
 }
 
 
+void createGeometry(Vertex* geometry, int* indices) {
+//    geometry = new Vertex[4];
+    glm::vec2 m_Position = {0.0f, 0.0f};
+    glm::vec2 m_WorldSize = {1.0f, 1.0f};
+
+    // top left
+    geometry[0].pos.x = m_Position.x - m_WorldSize.x / 2.0f;
+    geometry[0].pos.y = m_Position.y + m_WorldSize.y / 2.0f;
+    geometry[0].color.r = 0;
+    geometry[0].color.g = 20;
+    geometry[0].color.b = 0;
+    geometry[0].uv.u = 0.0f;
+    geometry[0].uv.v = 1.0f;
+
+    // bottom left
+    geometry[1].pos.x = m_Position.x - m_WorldSize.x / 2.0f;
+    geometry[1].pos.y = m_Position.y - m_WorldSize.y / 2.0f;
+    geometry[1].color.r = 255;
+    geometry[1].color.g = 255;
+    geometry[1].color.b = 255;
+    geometry[1].uv.u = 0.0f;
+    geometry[1].uv.v = 0.0f;
+
+    // bottom right
+    geometry[2].pos.x = m_Position.x + m_WorldSize.x / 2.0f;
+    geometry[2].pos.y = m_Position.y - m_WorldSize.y / 2.0f;
+    geometry[2].color.r = 255;
+    geometry[2].color.g = 255;
+    geometry[2].color.b = 255;
+    geometry[2].uv.u = 1.0f;
+    geometry[2].uv.v = 0.0f;
+
+    // top right
+    geometry[3].pos.x = m_Position.x + m_WorldSize.x / 2.0f + 0.1f;
+    geometry[3].pos.y = m_Position.y + m_WorldSize.y / 2.0f;
+    geometry[3].color.r = 255;
+    geometry[3].color.g = 255;
+    geometry[3].color.b = 255;
+    geometry[3].uv.u = 1.0f;
+    geometry[3].uv.v = 1.0f;
+
+//    indices = new int[6];
+    indices[0] = 3;
+    indices[1] = 0;
+    indices[2] = 1;
+    indices[3] = 1;
+    indices[4] = 2;
+    indices[5] = 3;
+}
+
+
 int main() {
 
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -255,10 +297,14 @@ int main() {
     if (!window)
         Logger::info("Couldn't create window, SDL_CreateWindow() returned nullptr");
 
+    Vertex* geometry = new Vertex[4];
+    int* indices = new int[6];
+    createGeometry(geometry, indices);
+    cout << "Main Thread - Geometry created !" << endl;
 
     RenderingThread renderingThread(window);
 
-
+    renderingThread.startRenderingLoop();
     while (gameState != GameState::EXIT) {
         handleInput();
     }
